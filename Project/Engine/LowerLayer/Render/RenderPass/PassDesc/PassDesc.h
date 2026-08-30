a9b860e9ff9d094a7e517b94216d9232b59053e2
miyaG10ma <k024g0106@g.neec.ac.jp>
2026-08-31 03:14:51 +0900

    passDescのカラーバッファの項目にblendModeを新設。ログの出力も追記

--- Project/Engine/LowerLayer/Render/RenderPass/PassDesc/PassDesc.h
+++ Project/Engine/LowerLayer/Render/RenderPass/PassDesc/PassDesc.h
@@ -8,27 +8,30 @@ private:
 
 	struct DataKeyString
 	{
-		std::string const kIsOffScreenB = "isOffScreen";
-		std::string const kMS = "ms";
-		std::string const kPS = "ps";
+		std::string const kIsOffScreenB			= "isOffScreen";
+		std::string const kMS					= "ms";
+		std::string const kPS					= "ps";
 
-		std::string const kColorFormatI = "colorFormat";
-		std::string const kClearColorV4 = "clearColor";
-		std::string const kColorWidthI = "colorWidth";
-		std::string const kColorHeightI = "colorHeight";
-		std::string const kNumBuffer_colorI = "numBuffer_Color";
+		std::string const kColorBufferName		= "colorBufferName";
+		std::string const kBlendMode			= "blendMode";
+		std::string const kColorFormatI			= "colorFormat";
+		std::string const kClearColorV4			= "clearColor";
+		std::string const kColorWidthI			= "colorWidth";
+		std::string const kColorHeightI			= "colorHeight";
+		std::string const kNumBuffer_colorI		= "numBuffer_Color";
 
-		std::string const kUseDepthStenciB = "useDepthStencil";
-		std::string const kDsvFormatI = "dsvFormat";
-		std::string const kSrvFormatI = "srvFormat";
-		std::string const kClearDepthF = "clearDepth";
-		std::string const kClearStencilI = "clearStencil";
-		std::string const kDepthWidthI = "depthWidth";
-		std::string const kDepthHeightI = "depthHeight";
-		std::string const kNumBuffer_depthI = "numBuffer_Depth";
+		std::string const kDepthBufferName		= "depthBufferName";
+		std::string const kUseDepthStenciB		= "useDepthStencil";
+		std::string const kDsvFormatI			= "dsvFormat";
+		std::string const kSrvFormatI			= "srvFormat";
+		std::string const kClearDepthF			= "clearDepth";
+		std::string const kClearStencilI		= "clearStencil";
+		std::string const kDepthWidthI			= "depthWidth";
+		std::string const kDepthHeightI			= "depthHeight";
+		std::string const kNumBuffer_depthI		= "numBuffer_Depth";
 
-		std::string const kDepthTestI = "depthTest";
-		std::string const kDepthEnableB = "depthEnable";
+		std::string const kDepthTestI			= "depthTest";
+		std::string const kDepthEnableB			= "depthEnable";
 
 	};
 
@@ -38,6 +41,7 @@ public:
 
 	PassDesc
 	(
+		std::string name_,
 		std::optional<std::pair<std::string, std::string >> ms_psFileName_,
 		RenderPassState renderPassState_,
 		std::vector<RenderContext::RequiredBufferInfo::ColorBuffer> colorBuffersInfo_,
@@ -53,6 +57,8 @@ public:
 	void SetColorBufferUniqueID(BufferUniqueID id_, size_t index_);
 	void SetDepthStencilBufferUniqueID(BufferUniqueID id_);
 
+	void DebugLog()const;
+
 private:
 
 	//オフスクリーンパスの場合、シェーダーファイルは1つに限定される
@@ -65,6 +71,8 @@ private:
 	std::vector<RenderContext::RequiredBufferInfo::ColorBuffer> colorBuffersInfo;
 	std::optional<RenderContext::RequiredBufferInfo::DepthStencilBuffer> depthStencilBufferInfo;
 
+	//デバッグ用
+	std::string passName;
 
 };
 
--- Project/Engine/LowerLayer/Render/RenderStateComponent.h
+++ Project/Engine/LowerLayer/Render/RenderStateComponent.h
@@ -2,12 +2,16 @@
 
 namespace RenderStateComponent
 {
+    //追加したら下の変換関数もね
     enum class BlendMode
     {
         kOpaque,
         kAlphaBlend,
         kAdditive
-        ,kCount
+        ,kCount,
+
+        ///RenderPass専用！！！
+        kDependsModel,
     };
 
     enum class FillMode
@@ -25,6 +29,35 @@ namespace RenderStateComponent
         ,kCount
     };
 
+    inline BlendMode const BlendModeToEnum(std::string blendModeString_)
+    {
+        BlendMode blendMode = BlendMode::kCount;
+
+        if      (blendModeString_ == "kOpaque")         blendMode = BlendMode::kOpaque;
+        else if (blendModeString_ == "kAlphaBlend")     blendMode = BlendMode::kAlphaBlend;
+        else if (blendModeString_ == "kAdditive")       blendMode = BlendMode::kAdditive;
+        else if (blendModeString_ == "kDependsModel")   blendMode = BlendMode::kDependsModel;
+
+        
+        ErrorMessageOutput::Assert::DetectError(blendMode != BlendMode::kCount, "ブレンドモードの文字変換失敗", "RenderStateComponent.h");
+        
+        return blendMode;
+    }
+
+    inline const std::string BlendModeToString(BlendMode mode_)
+    {
+        std::string blendMode = "";
+
+        if      (mode_ == BlendMode::kOpaque)            blendMode = "kOpaque";
+        else if (mode_ == BlendMode::kAlphaBlend)        blendMode = "kAlphaBlend";
+        else if (mode_ == BlendMode::kAdditive)          blendMode = "kAdditive";
+        else if (mode_ == BlendMode::kDependsModel)      blendMode = "kDependsModel";
+
+        ErrorMessageOutput::Assert::DetectError(blendMode.size()> 0 , "ブレンドモードの文字変換失敗", "RenderStateComponent.h");
+
+        return blendMode;
+    }
+
     inline D3D12_FILL_MODE Convert(FillMode fillMode_)
     {
         return (fillMode_ == FillMode::kSolid) ? D3D12_FILL_MODE_SOLID : D3D12_FILL_MODE_WIREFRAME;
@@ -34,8 +67,8 @@ namespace RenderStateComponent
     {
         D3D12_CULL_MODE cullMode = D3D12_CULL_MODE_BACK;
 
-        if (cullMode_ == CullMode::kFront) cullMode = D3D12_CULL_MODE_FRONT;
-        else if (cullMode_ == CullMode::kNone) cullMode = D3D12_CULL_MODE_NONE;
+        if      (cullMode_ == CullMode::kFront) cullMode = D3D12_CULL_MODE_FRONT;
+        else if (cullMode_ == CullMode::kNone)  cullMode = D3D12_CULL_MODE_NONE;
 
         return cullMode;
     }
