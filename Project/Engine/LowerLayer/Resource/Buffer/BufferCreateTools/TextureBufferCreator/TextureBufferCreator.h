9c430b14abf479e11d8503dc5bbfefda087f691d
miyaG10ma <k024g0106@g.neec.ac.jp>
2026-09-04 16:59:59 +0900

    テクスチャバッファの作成機構の外枠構築

--- Project/Engine/LowerLayer/Resource/Buffer/BufferCreateTools/TextureBufferCreator/TextureBufferCreator.h
+++ Project/Engine/LowerLayer/Resource/Buffer/BufferCreateTools/TextureBufferCreator/TextureBufferCreator.h
@@ -3,6 +3,22 @@
 
 class BufferContext::TextureBufferCreator
 {
+	//テクスチャファイルを読み込み、
+	class TextureLoader;
+	//パルスして、テクスチャバッファのDescを作る
+	class TextureBufferDescAssembler;
+
+public:
+	TextureBufferCreator
+	(
+		NexusFieldProof proof_,
+		BufferCreator& bufferCreator_,
+		BufferCollector& bufferCollector_,
+		BufferUploader& bufferUploader_,
+		GlobalConstantBufferCreator& globalConstantBufferCreator_,
+		TextureLoader& textureLoader_,
+		TextureBufferLibrary& textureBufferLibrary_
+	);
 
 };
 
---
+++ Project/Engine/LowerLayer/Resource/Buffer/TextureBufferLibrary/TextureBufferLibrary.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "../BufferContext.h"
+
+class BufferContext::TextureBufferLibrary
+{
+public:
+	TextureBufferLibrary(NexusFieldProof proof_);
+
+
+private:
+
+
+};
+
